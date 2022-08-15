3a6f8f03f46df33cb2a4a23c53983a3b4614d446
Lautaro Carrasco <lautaronicolas1996@gmail.com>
2022-08-15 18:16:04 -0300

    Creo repositorio React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hola mundo!</h1>
+    <h3>Primera entrega</h3>
+    </>
+  );
+}
+
+export default App;
